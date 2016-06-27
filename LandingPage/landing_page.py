c07153d5c232ba112ef2086562feb5e29324961e
Steve Duncan <steve.duncan.email@gmail.com>
2016-06-27 14:18:00 -0700

    added new dojo form to Flask landing page

--- LandingPage/landing_page.py
+++ LandingPage/landing_page.py
@@ -1,9 +1,19 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
-@app.route('/')
+# @app.route('/')
+# def greeting():
+# 	return render_template('index.html',name = 'Steve')
+# app.run(debug=True)
 
-def greeting():
-	return render_template('index.html',name = 'Steve')
 
+# @app.route('/ninjas')
+# def ninjas():
+# 	return render_template('ninjas.html')
+# app.run(debug=True)
+
+
+@app.route('/dojos/new')
+def dojos():
+	return render_template('new_dojos.html')
 app.run(debug=True)
