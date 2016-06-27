3dc3f22a823ef9a94737cfe48d34103716b6465a
Steve Duncan <steve.duncan.email@gmail.com>
2016-06-27 15:03:20 -0700

    added styles to new dojo form for Flask landing page

--- LandingPage/landing_page.py
+++ LandingPage/landing_page.py
@@ -1,18 +1,19 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+#part 1
 # @app.route('/')
 # def greeting():
 # 	return render_template('index.html',name = 'Steve')
 # app.run(debug=True)
 
-
+#part 2
 # @app.route('/ninjas')
 # def ninjas():
 # 	return render_template('ninjas.html')
 # app.run(debug=True)
 
-
+#part 3
 @app.route('/dojos/new')
 def dojos():
 	return render_template('new_dojos.html')
